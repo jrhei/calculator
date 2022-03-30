0b71c8a0a730a56be9b49a36a99de04ef7c54d32
jrhei <jannorheydon@gmail.com>
2022-03-30 11:18:04 +0800

    BETA 0.03 * Change buttons from edit and input to ID instead of class * Add queryselectors to be able to distinguish which button is pressed * Change stylesheet to support the class to ID changes

--- javascript.js
+++ javascript.js
@@ -10,3 +10,14 @@ function multiply(a, b){
 function divide(a, b){
     return a/b;
 }
+
+function operate(operator, a ,b){
+    return operator(a, b);  
+}
+
+const button = document.querySelectorAll('button');
+button.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.id);
+    })
+})
