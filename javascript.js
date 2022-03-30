377c8893ad37274431eb046d3044020505d6d3fa
jrhei <jannorheydon@gmail.com>
2022-03-30 12:52:08 +0800

    BETA 0.04 * Add function that adds numbers and operators to display div depending on which button is pressed

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+const output = document.createElement('div');
+output.classList.add('output');
+const display = document.querySelector('.display')
 function add(a, b){
     return a + b;
 }
@@ -18,6 +21,75 @@ function operate(operator, a ,b){
 const button = document.querySelectorAll('button');
 button.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(button.id);
+        let input = button.id;
+        console.log(input);
+        if (input === '0'){
+            output.textContent += '0';
+            display.appendChild(output);
+        }
+        else if (input === '.'){
+            output.textContent += '.';
+            display.appendChild(output);
+        }
+        else if (input === '+'){
+            output.textContent += '+';
+            display.appendChild(output);
+        }
+        else if (input === '1'){
+            output.textContent += '1';
+            display.appendChild(output);
+        }
+        else if (input === '2'){
+            output.textContent += '2';
+            display.appendChild(output);
+        }
+        else if (input === '3'){
+            output.textContent += '3';
+            display.appendChild(output);
+        }
+        else if (input === '-'){
+            output.textContent += '-';
+            display.appendChild(output);
+        }
+        else if (input === '4'){
+            output.textContent += '4';
+            display.appendChild(output);
+        }
+        else if (input === '5'){
+            output.textContent += '5';
+            display.appendChild(output);
+        }
+        else if (input === '6'){
+            output.textContent += '6';
+            display.appendChild(output);
+        }
+        else if (input === '*'){
+            output.textContent += '*';
+            display.appendChild(output);
+        }
+        else if (input === '7'){
+            output.textContent += '7';
+            display.appendChild(output);
+        }
+        else if (input === '8'){
+            output.textContent += '8';
+            display.appendChild(output);
+        }
+        else if (input === '9'){
+            output.textContent += '9';
+            display.appendChild(output);
+        }
+        else if (input === '/'){
+            output.textContent += '/';
+            display.appendChild(output);
+        }
+        else if (input === 'clear'){
+            output.textContent = '';
+            display.appendChild(output);
+        }
+        else if (input === 'delete'){
+            console.log(output.textContent)
+        }
     })
 })
+
